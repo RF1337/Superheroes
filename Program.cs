fa2ccc02e5521f616d2b06a81f77a969bd0b3484
RF1337 <112627787+RF1337@users.noreply.github.com>
2023-02-06 09:41:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Superhero
+{
+    class Superhero
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _heroOutfit;
+
+        public string HeroOutfit
+        {
+            get { return _heroOutfit; }
+            set { _heroOutfit = value; }
+        }
+
+        private string _power;
+
+        public string Power
+        {
+            get { return _power; }
+            set { _power = value; }
+        }
+
+        private string _archNemesis;
+
+        public string ArchNemesis
+        {
+            get { return _archNemesis; }
+            set { _archNemesis = value; }
+        }
+    }
+}
